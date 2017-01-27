f8b7b277b2485e3f9d59ca6061b142923e80ebbd
Steve Bertrand <steveb@cpan.org>
2017-01-27 16:01:02 -0700

    added register information to pod

--- lib/RPi/DAC/MCP49XX.pm
+++ lib/RPi/DAC/MCP49XX.pm
@@ -24,6 +24,27 @@ converters (DAC) over the SPI bus
 
 =head1 TECHNICAL INFORMATION
 
+=head2 DEVICE REGISTER
+
+The write register is the same for all devices under the MCP49xx umbrella, with
+the differing devices having differing sizes for the data portion. Following is
+a diagram that depicts the register for the different devices, where C<x> shows
+that the bit is available, with a C<-> signifying that this bit will be ignored.
+Note that a full 16-bits needs to be sent in regardless of chip type.
+
+            |<---------------- Write Command Register --------------->|
+            |                   |                                     |
+            |<---- control ---->|<------------ data ----------------->|
+            ] 15 | 14 | 13 | 12 | 11 10 09 08 07 06 05 04 03 02 01 00 |
+            -----------------------------------------------------------
+            | ^  |  ^ |  ^ |  ^ |               ^                     |
+            | |     |    |    | |               |                     |
+            |A/B | BUF|GAIN|SHDN|              DATA                   |
+            |---------------------------------------------------------|
+    MCP4922 | x  |  x |  x |  x |  x  x  x  x  x  x  x  x  x  x  x  x |
+    MCP4912 | x  |  x |  x |  x |  x  x  x  x  x  x  x  x  x  x  -  - |
+    MCP4902 | x  |  x |  x |  x |  x  x  x  x  x  x  x  x  -  -  -  - |
+            -----------------------------------------------------------
 
 =head1 AUTHOR
 
