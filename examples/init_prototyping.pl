5580b09473dc9579179f40f5ad1672a1e7b1e973
Steve Bertrand <steveb@cpan.org>
2017-01-27 17:58:36 -0700

    working on the prototype code

--- examples/init_prototyping.pl
+++ examples/init_prototyping.pl
@@ -4,6 +4,7 @@ use feature 'say';
 
 use Bit::Manip qw(:all);
 use RPi::WiringPi;
+use RPi::WiringPi::Constant qw(:all);
 
 my $spi_chan = 0;
 
@@ -12,21 +13,41 @@ my $adc = $pi->adc;
 my $spi = $pi->spi($spi_chan);
 
 my $cs_pin = $pi->pin(18);
+my $shdn_pin = $pi->pin(6);
+
+# turn the shutdown pin to HIGH
+
+$shdn_pin->mode(OUTPUT);
+$shdn_pin->write(HIGH);
+
+ # say $shdn_pin->read;
 
 # drop chip select to LOW to start conversation with the DAC
 
 $cs_pin->write(LOW);
+ # say $cs_pin->read;
 
-# bit manip to do some prototype testing
+# create the 16-bit register, and set the MSB
 
-my $register = bit_set(0, 0, 16, 0);
-say $register;
+my $register = bit_set(0, 0, 16, 32768);
+say bit_bin($register);
+
+# set the gain register bit
+
+$register = bit_set($register, 13, 1, 1);
+say bit_bin($register);
+
+# set data
+
+$register = bit_set($register, 0, 12, 4095);
+say bit_bin($register);
+
+# go HIGH to tell the IC we're done clocking in bits
+
+$cs_pin->write(HIGH);
+
 say $adc->percent;
 
-
-# put chip select to HIGH to end conversation with the DAC
-
-$cs_pin->write(LOW);
 
 
 
